fe2b08e71ad0e5ee53191d9d5b9310fb809cf42c
melissa kobia <melissakobia1997@gmail.com>
2019-08-18 16:26:31 +0300

    Adds a function that calculates the of the week a user was born

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 var date = parseInt(document.getElementById("date"));
 var month = parseInt(document.getElementById("month"));
-var year = parseInt(document.getElementById("year"));
+var year = document.getElementById("year");
 var male = document.getElementById("male");
 var female = document.getElementById("female");
 var femaleArray = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 var maleArray = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var cc = parseInt(year.slice(0,2));
+var yy = parseInt(year.slice(2,4));
+var d = function(date,month,cc,yy){
+  return  ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + date ) % 7;
+
+}
