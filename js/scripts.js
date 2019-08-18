8ab44df08fec7cf2791e08c652a11b8776baabfa
melissa kobia <melissakobia1997@gmail.com>
2019-08-18 11:42:30 +0300

    Adds variables to the scripts file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var date = document.getElementById("date");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
+var male = document.getElementById("male");
+var female = document.getElementById("female");
